36d312f12e47c8bd85ae20604c8edc00b86a3735
Chandana141 <chandanadhanapal25@gmail.com>
2021-03-30 10:57:52 +0000

    done

--- Program-5.c
+++ Program-5.c
@@ -13,8 +13,8 @@ int main()
   int c = 0, count = 0;
   char s[1000];
 
-  printf("Input a string\n");
-  gets(s);
+  printf("Input a string");
+  scanf("%s",s);
 
   while (s[c] != '\0') {
     if (s[c] == 'a' || s[c] == 'A' || s[c] == 'e' || s[c] == 'E' || s[c] == 'i' || s[c] == 'I' || s[c] =='o' || s[c]=='O' || s[c] == 'u' || s[c] == 'U')
